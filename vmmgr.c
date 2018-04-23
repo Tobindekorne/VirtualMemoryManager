4a9d74ed25d95dd9a595d9aec378d9dae7720200
Tobindekorne <tdekorne@gmail.com>
2018-04-22 18:25:57 -0700

    Added more descriptive comments throughout and removed redundant array for virtual memory in favor of more descriptive page table array

--- vmmgr.c
+++ vmmgr.c
@@ -9,7 +9,6 @@
 
 //Define some constant values having to do with system properties
 #define PAGE_LENGTH 256 //The maximum length of each page (256 bytes)
-#define VM_SIZE 256		//The maximum size of virtual memory
 #define FRAME_SIZE 256	//The maximum size of a frame in physical memory
 #define NUM_FRAMES 256	//The maximum number of frames this system supports
 #define TLB_SIZE 16		//The maximum number of TLB entries
@@ -23,25 +22,8 @@ int getPageOffset(int logicalAdd);
 int readBackingStore();
 void translateAddress(int arr[]);
 void initialize(int arr[], size_t size);
-
-
-void bin(unsigned n)
-{
-    unsigned i;
-    for (i = 1 << 31; i > 0; i = i / 2)
-        (n & i) ? printf("1"): printf("0");
-	printf("\n");
-}
-
-void bin2(char * arr) {
-	int j, i;
-	for (j = 0; j < 256; j++) {
-		for (i = 0; i < 8; i++) {
-			printf("%d", !!((arr[j] << i) & 0x80));
-		}
-		printf("\n");
-	}
-}
+void bin(unsigned n);
+void bin2(char * arr);
 
 //
 //Start of the main program
@@ -63,13 +45,14 @@ int main(int argc, char const *argv[]) {
 
 	//initialize arrays for ram, virtual memory, and the TLB
 	int ram[FRAME_SIZE * NUM_FRAMES]; 	//set up ram to have 256 frames with 256 bytes each
+    //set page table size at 256 bytes - one for each frame in RAM
 	int pageTable[PAGE_LENGTH];
-	int vm[VM_SIZE];					//set virtual memory size at 256 bytes
-	int tlb[TLB_SIZE];					//set the TLB to have 16 slots
+    //set the TLB to have 16 entries and 2 slots for each entry
+    // one slot for the page number and one for the frame number
+	int tlb[TLB_SIZE][2];
 
 	//Use the initialize function to initialize each table with blank memory (-1 in each slot)
 	initialize(ram, FRAME_SIZE * NUM_FRAMES);
-	initialize(vm, VM_SIZE);
 	initialize(tlb, TLB_SIZE);
 	initialize(pageTable, PAGE_LENGTH);
 
@@ -94,7 +77,7 @@ int main(int argc, char const *argv[]) {
 	//using the functions written for dealing with a paging system
 	while (fgets(logicalAdd, 7, addFD) != NULL) {
 		logicalAddress = atoi(logicalAdd);
-		vm[index] = logicalAddress;
+		pageTable[index] = logicalAddress;
 		index++;
 	}
 
@@ -105,14 +88,23 @@ int main(int argc, char const *argv[]) {
 	// }
 
 	//Translate addresses
-	//translateAddress(vm);
+	//translateAddress(pageTable);
 
 	//Process
 
 	return EXIT_SUCCESS;
 }
 
-//This function will initialize an integer array with every slot set to -1 to denote uninitialized memory
+/*
+ * Function:  initialize
+ * --------------------
+ * initialize an integer array with every
+ * entry set to -1 to denote uninitialized memory
+ *
+ *  arr: the int array to be initialized
+ *  size: the size of the array
+ *
+ */
 void initialize(int arr[], size_t size) {
 	int i = 0;
 	for (i = 0; i < size; i++) {
@@ -120,13 +112,35 @@ void initialize(int arr[], size_t size) {
 	}
 }
 
-//This function returns the page number from a logical address
-//params: logicalAdd an integer logical address
-//return: int - the 8 bit representation of the page number
+/*
+ * Function:  getPageNumber
+ * --------------------
+ * returns the page number from a logical address
+ *
+ *  logicalAdd: the logical address to get the offset from as an int
+ *
+ *  returns: the 8 bit representation of the page number
+ */
 int getPageNumber(int logicalAdd) {
 	return logicalAdd >> 8; //shift the bits right 8 times to place the page number in the correct location
 }
 
+
+/*
+ * Function:  outputData
+ * --------------------
+ * outputs the logical address being translated, the physical address it
+ * maps to, the signed byte for that value, the page number,
+ * and the page offset.
+ *
+ *  logicalAdd: the logical address as an int
+ *  physicalAdd: the mapped physical address as an int
+ *  signedByte: the signed byte value there
+ *  page: the page that goes with the logical address
+ *  offset: the offset into the frame for the physical address
+ *
+ *  returns: the 8 bit representation of the page number
+ */
 void outputData(int logicalAdd, int physicalAdd, char signedByte, unsigned char page, unsigned char offset) {
 	char *head_foot = "---------------------------\n";
 	printf("%s\n", head_foot);
@@ -138,14 +152,29 @@ void outputData(int logicalAdd, int physicalAdd, char signedByte, unsigned char 
 	printf("%s\n", head_foot);
 }
 
-//This function returns the page offset from a logical address
-//params: logicalAdd an integer logical address
-//return: int - the 8 bit representation of the page offset
+/*
+ * Function: getPageOffset
+ * --------------------
+ * computes the page offset of a virtual address:
+ *
+ *  logicalAdd: the logical address to get the offset from as an int
+ *
+ *  returns: the 8 bit representation of the page offset
+ */
 int getPageOffset(int logicalAdd) {
 	return logicalAdd & 0xFF; //bit mask the logical address with hex FF to get the last bits of data
 }
 
-//This is the function that translates logical addresses to physical addresses
+/*
+ * Function:  translateAddress
+ * --------------------
+ * translates a virtual (logical) address to a physical address using:
+ *          [page # as first 8 bits][page offset as second 8 bits]
+ *  virtualAddress: the virtual (logical) address to be translated
+ *
+ *  returns: the frame number of the address
+ *           returns -1 if there is a page fault
+ */
 void translateAddress(int arr[]) {
 	int i = 0; //indexing value
 	unsigned char page, offset; //variables for holding the page number and page offset
@@ -161,10 +190,51 @@ void translateAddress(int arr[]) {
 	}//end while
 }
 
-//printTable prints the contents of any table as an integer array
+/*
+ * Function:  printTable
+ * --------------------
+ * outputs all values held in a the page table
+ *
+ *  arr: the integer array of page table entries
+ *  size: the size of the page table array
+ *
+ */
 void printTable(int arr[], size_t size) {
 	int i = 0;
 	for (i; i < size; i++) {
 		printf("%i %i\n", i, arr[i]); //print the position in the table and the contents at that location
 	}//end for
+}
+
+/*
+ * Function:  bin
+ * --------------------
+ * outputs all binary value of an unsigned value n
+ *
+ *  n: an unsigned vlaue
+ */
+void bin(unsigned n)
+{
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+        (n & i) ? printf("1"): printf("0");
+	printf("\n");
+}
+
+/*
+ * Function:  bin2
+ * --------------------
+ * outputs all binary values
+ * from an array of characters as one byte per line
+ *
+ *  arr: the character array
+ */
+void bin2(char * arr) {
+	int j, i;
+	for (j = 0; j < 256; j++) {
+		for (i = 0; i < 8; i++) {
+			printf("%d", !!((arr[j] << i) & 0x80));
+		}
+		printf("\n");
+	}
 }
